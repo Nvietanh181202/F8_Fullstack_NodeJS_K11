2a94680c19f15aaa6b3736a94efdb8b91ef2931f
unknown <nv.anh.181202@gmail.comgit config --global user.name Nvietanh181202git branch -M maingit config --global user.email nv.anh.181202@gmail.com>
2024-12-30 00:29:59 +0700

    nộp bài tập buổi 25

---
+++ home_work/homework-day-25/ex03.js
@@ -0,0 +1,53 @@
+/**
+ * Add a custom reduce2 method to Array.prototype
+ * This method mimics the behavior of Array.prototype.reduce
+ * @param {function} callback - The function to execute for each element in the array.
+ * @param {any} [initialValue] - Optional. Value to use as the first argument to the first call of the callback.
+ * @returns {any} - The value that results from the reduction.
+ */
+
+Array.prototype.reduce2 = function (callback, initialValue) {
+  if (this.length === 0 && initialValue === undefined) {
+    return " Reduce of empty array with no initial value";
+  }
+  let accumulator = initialValue === undefined ? this[0] : initialValue;
+  let i = initialValue === undefined ? 1 : 0;
+  for (; i < this.length; i++) {
+    if (i in this) {
+      accumulator = callback(accumulator, this[i], i, this);
+    }
+  }
+  return accumulator;
+};
+
+// Sample usage
+
+// Sample 1
+
+const arr1 = [1, 2, 3, 4, 5];
+
+const result1 = arr1.reduce2((accumulator, value) => {
+  return accumulator + value;
+}, 0);
+
+console.log(result1); // 15
+
+// Sample 2
+
+const arr2 = [];
+
+const result2 = arr2.reduce2((accumulator, value) => {
+  return accumulator + value;
+});
+
+console.log(result2); // Error: Reduce of empty array with no initial value
+
+// Sample 3
+
+const arr3 = [];
+
+const result3 = arr3.reduce2((accumulator, value) => {
+  return accumulator + value;
+}, 0);
+
+console.log(result3); // 0
