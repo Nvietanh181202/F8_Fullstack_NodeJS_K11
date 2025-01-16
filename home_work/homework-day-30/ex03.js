d37a974a9bcab515817612d052f59e1e9fefdb04
unknown <nv.anh.181202@gmail.comgit config --global user.name Nvietanh181202git branch -M maingit config --global user.email nv.anh.181202@gmail.com>
2025-01-17 00:34:59 +0700

    nộp bài tập buổi 30

--- home_work/homework-day-30/ex03.js
+++ home_work/homework-day-30/ex03.js
@@ -1,6 +1,11 @@
 const countElements = (tagname) => {
+  if (typeof tagname !== "string" || tagname === "") {
+    return "Invalid";
+  }
   let elements = document.getElementsByTagName(tagname);
   return elements.length;
 };
 console.log(countElements("div"));
 console.log(countElements("p"));
+console.log(countElements(""));
+console.log(countElements(123));
